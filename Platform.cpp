0166371d9a6904ee6603a66e2b517478dd93bc0e
Filippo Speziali <79914413+CiumbaSpe@users.noreply.github.com>
2021-03-19 23:24:08 +0100

    Update Platform.cpp
    ora la funzione di stampa è perfetta, funziona sia avanti che indietro in modo stra smooth e con n platform presenti. lenS è la lunghezza del campo, bisogno aggiustare forse la funzione add node e aggiungere una funzione di generazione e poi la classe è completa

--- Platform.cpp
+++ Platform.cpp
@@ -10,9 +10,10 @@ Platform::Platform(){
         current = NULL;
 }
 
-void Platform::addNode(){ 
+
+void Platform::addNode(int h){ // ======================================================
         srand(time(NULL));
-        // se non ci sono ancora nodi
+        // se non ci sono ancora nodi -------------------------------------------
         if(first == NULL){
                 first = new node;
                 first -> x = 40; // un valore a caso che può essere anche k
@@ -24,35 +25,46 @@ void Platform::addNode(){
                 last = first;
                 current = first;
         }
-        else { // altrimenti aggiungo in testa
+        else { // altrimenti aggiungo in testa  --------------------------------
                 p_node tmp = new node;
+                last -> next = tmp;
                 tmp -> prev = last;
                 tmp -> next = NULL;
+                tmp -> x = tmp -> prev -> x + tmp -> prev -> len + rand() % 20 + 10;
+                do {
+                        tmp -> y = tmp -> prev -> y + rand() % 10 - rand() % 10;
+                } while(tmp -> y < 0 || tmp -> y > h);
+                tmp -> len = rand() % 10 + 5;
                 last = tmp;
                 tmp = NULL;
                 delete tmp;
         }
 
-}
+} // fine funzione addNode() ==========================================================
 
-void Platform::printPlatforms(int ps){
 
-        bool screen_void = false;
 
-        if(current -> x + current -> len < ps){
-                if(current->next != NULL)
-                        current = current->next;
-                else screen_void = true;
-        }
-        if(current -> x < ps + 100 && !screen_void){ // 100 è la larghezza del mio schermo (a caso)
-                // comunque di base se entri qua dentro vuol dire che stampi cose
 
-                for(int i = 0; i < current->len; i++){
-                        if(current -> x + i >= ps){
-                                mvprintw(current->y, current->x + i - ps,"=");
-                        }
+void Platform::printPlatforms(int ps, int lenS){ // ===================================
+
+        // 1) verifica dell'aggiornamento valore current -------------------------
+                // se sto andando indietro:
+        if(current -> x + current -> len < ps)
+                current = current -> next;
+                // se sto andando in avanti:
+        else if(current -> prev != NULL && current -> prev -> x + current -> prev -> len >= ps)
+                current = current -> prev;
+
+        // 2) stampare da current fino a limite schermo --------------------------
+        p_node iter = current;
+
+        while(iter != NULL && iter -> x < ps + lenS){ // cicla fino a che la nuova x di iter è fuori dallo schermo
+                for(int i = 0; i < iter->len; i++){
+                        if(iter -> x + i >= ps && iter -> x + i < ps + lenS)
+                                mvprintw(iter -> y, iter -> x + i - ps, "=");
                 }
-
+                iter = iter->next;
         }
-}
+
+} // fine funzione printPlatforms() ==================================================
 
