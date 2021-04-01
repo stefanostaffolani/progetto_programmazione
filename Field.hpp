bc2d8e7b6e7411237f89df30b6d120cc4dda3aa7
Filippo Speziali <79914413+CiumbaSpe@users.noreply.github.com>
2021-04-01 16:03:15 +0200

    Update Field.hpp

--- Field.hpp
+++ Field.hpp
@@ -4,8 +4,14 @@ public:
 	void printField();
 	void upgradeData(int plLife, int plPoints);
 
+	int getLenS() { return lenS; }
+	int getHeight() { return height; }
+	
 protected:
 	int i;
 	int life;
 	int points;
+	
+	int lenS; // length screen
+	int height; // height of the screen
 };
