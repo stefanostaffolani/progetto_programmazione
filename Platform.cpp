2bc0e7abaa960d72687d1a691e6a99575da85493
Filippo Speziali <79914413+CiumbaSpe@users.noreply.github.com>
2021-04-01 16:02:43 +0200

    Update Platform.cpp

--- Platform.cpp
+++ Platform.cpp
@@ -17,7 +17,7 @@ void Platform::addNode(int h){ // ==============================================
         if(first == NULL){
                 first = new node;
                 first -> x = 40; // un valore a caso che può essere anche k
-                first -> y = rand() % 10 + 1;  // siccome il player deve poterci saltare do valore rand
+                first -> y = rand() % 10 + 3;  // siccome il player deve poterci saltare do valore rand
                 //first -> len = rand() % 10 + 5;
                 first -> len = 10;
                 first -> prev = NULL;
@@ -33,7 +33,7 @@ void Platform::addNode(int h){ // ==============================================
                 tmp -> x = tmp -> prev -> x + tmp -> prev -> len + rand() % 20 + 10;
                 do {
                         tmp -> y = tmp -> prev -> y + rand() % 10 - rand() % 10;
-                } while(tmp -> y < 0 || tmp -> y > h);
+                } while(tmp -> y < 3 || tmp -> y > h);
                 tmp -> len = rand() % 10 + 5;
                 last = tmp;
                 tmp = NULL;
@@ -56,7 +56,7 @@ void Platform::printPlatforms(int ps, int lenS){ // ============================
                 current = current -> prev;
 
         // 2) stampare da current fino a limite schermo --------------------------
-        p_node iter = current;
+        p_node iter = current;   void generate(int n);   // genera n nuove platform
 
         while(iter != NULL && iter -> x < ps + lenS){ // cicla fino a che la nuova x di iter è fuori dallo schermo
                 for(int i = 0; i < iter->len; i++){
@@ -68,3 +68,12 @@ void Platform::printPlatforms(int ps, int lenS){ // ============================
 
 } // fine funzione printPlatforms() ==================================================
 
+
+
+
+void Platform::generate(int height, int lenS, int ps, int n){
+        if(last->x < ps + lenS){ // genera platform se l'ultima entra nello schermo
+                for(int i = 0; i < n; i++)
+                      addNode(height);
+        }
+}
