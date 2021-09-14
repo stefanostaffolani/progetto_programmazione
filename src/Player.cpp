cedc675a661a947081c2d9d49d0f3bfb1e790f96
stefanostaffolani <staffolani.stefano@gmail.com>
2021-09-14 18:23:26 +0200

    sistemato codice su player e risolto bug sul bonus

--- src/Player.cpp
+++ src/Player.cpp
@@ -3,7 +3,12 @@
 #include "Player.hpp"
 
 using namespace std;
- 
+
+#define SPACE 32
+#define EQUAL 61
+#define DOLLAR 36
+#define PIPE 124
+
 Player::Player(char avatar, Platform* p1, Bonus* b1, int x, int y):Item(avatar, p1, b1, x, y){
     life = 100;
     points = 0;
@@ -33,7 +38,8 @@ void Player::jump(int& ps, p_bullet& head){
         c = getch();
         if(c == 32) head = add_bullet(head, this->pos, this->versor);
         if (mvinch(this->pos.y - 1, this->pos.x + versor) == 32){
-            mvprintw(this->pos.y, this->pos.x, " ");
+            this->delete_item();
+            //mvprintw(this->pos.y, this->pos.x, " ");
             this->pos.y--;
             this->move(ps);
             i++;
@@ -41,7 +47,6 @@ void Player::jump(int& ps, p_bullet& head){
             hit_something = true;
         }
         print_bullet_list(head);
-        //std::this_thread::sleep_for(std::chrono::milliseconds (100));
     }
     gravity(ps, head);
 }
@@ -50,37 +55,52 @@ void Player::gravity(int& ps, p_bullet& head){
     bool hit_something = false;
     //int i = 0;
     char c;
-    if (mvinch(this->pos.y + 1, this->pos.x) != 32) hit_something = true;
+    if (mvinch(this->pos.y + 1, this->pos.x) != SPACE) hit_something = true;
     else hit_something = false;
+    position diagonale, sotto;
     while(!(hit_something)){
         timeout(100);
+        
+        //per sparare
         c = getch();
-        if(c == 32) head = add_bullet(head, this->pos, this->versor);
-        if(mvinch(this->pos.y + 1, this->pos.x + versor) != 32){
+        if(c == SPACE) head = add_bullet(head, this->pos, this->versor);
+        
+        diagonale = {this->pos.x + versor, this->pos.y + 1};
+        sotto = {this->pos.x, this->pos.y+1};
+
+        if (mvinch(sotto.y, sotto.x) == EQUAL || mvinch(sotto.y, sotto.x) == 45){   // ho qualcosa sotto
             hit_something = true;
-            if (mvinch(this->pos.y + 1, this->pos.x + versor) == 36){
-                mvprintw(this->pos.y, this->pos.x, " ");
+        }
+        else if(mvinch(diagonale.y, diagonale.x) != SPACE){
+            hit_something = true;
+            if (mvinch(diagonale.y, diagonale.x) == DOLLAR){
+                this->delete_item();
+                //mvprintw(this->pos.y, this->pos.x, " ");
                 this->pos.y++;
-                move(ps);
-                    //increase_points(10);
-                    //if (b2->findCash(ps, 75, this->x, this->y)) increase_points(10);
-            }else if(mvinch(this->pos.y + 1, this->pos.x + versor) == 124){
-                mvprintw(this->pos.y, this->pos.x, " ");
+                this->move(ps);
+                //increase_points(10);
+                //if (b2->findCash(ps, 75, this->x, this->y)) increase_points(10);
+            }else if(mvinch(diagonale.y, diagonale.x) == PIPE){
+                this->delete_item();
+                //mvprintw(this->pos.y, this->pos.x, " ");
                 this->pos.y = HEIGHT;
                 this->pos.x = INIT_X;
-                mvprintw(this->pos.y, this->pos.x, "%c", avatar);
+                this->print_item();
+                //mvprintw(this->pos.y, this->pos.x, "%c", avatar);
                 refresh();
-            }else if (mvinch(this->pos.y + 1, this->pos.x) == 32){
-                mvprintw(this->pos.y, this->pos.x, " ");
+            }else if (mvinch(sotto.y, sotto.x) == SPACE){
+                this->delete_item();
+                //mvprintw(this->pos.y, this->pos.x, " ");
                 this->move(ps);
             }
             //vedere se ne servono altri
-        }else if (mvinch(this->pos.y + 1, this->pos.x) == 61){
-            hit_something = true;
-        }else{
-            mvprintw(this->pos.y, this->pos.x, " ");
+        }
+        else{
+            this->move(ps);
+            this->delete_item();
+            //mvprintw(this->pos.y, this->pos.x, " ");
             this->pos.y++;
-            move(ps);
+            this->print_item();
         }
         print_bullet_list(head);
     }
