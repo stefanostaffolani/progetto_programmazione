2782e3e9365b1a70d62ef9c19e343bed4212a20f
stefanostaffolani <staffolani.stefano@gmail.com>
2021-09-23 18:07:00 +0200

    pre merge

--- src/Enemies.cpp
+++ src/Enemies.cpp
@@ -2,16 +2,15 @@
  
 Enemies::Enemies(){       // skeleton, solo alcune possibili funzioni per Enemies
     this->difficulty = 0;
-    this->fattore_incremento = 10;    // valutare poi il fattore di incremento
+    this->fattore_incremento = 200;    // valutare poi il fattore di incremento
 }
 
 void Enemies::increase_difficulty(int ps){    // operazione per aumentare la difficoltà dei nemici lungo il gioco (da valutare)
-    if(this->difficulty < 9){
+    if(this->difficulty < 9 && (this->difficulty < (this->difficulty + (ps / this->fattore_incremento)))){
         this->difficulty = this->difficulty + (ps / this->fattore_incremento);
     }
 }
 
-<<<<<<< HEAD
 
 void Enemies::generate(){
     // 
@@ -24,9 +23,6 @@ void Enemies::generate(){
 
 
 //per la frequenza di sparo si potrebbe fare (10 - difficult) (10 livelli di difficoltà), al decimo livello spara sempre
-=======
-//per la frequenza di sparo si potrebbe fare (10 - difficulty) (10 livelli di difficoltà), al decimo livello spara sempre
->>>>>>> 613e192f4b979c0576f9b8917ee194de22519606
 
 //TODO:rifare questa funzione per lavorare con la lista
 // void Enemies::generate_on_platform(int x){   // non so se sia necessaria la x come argomento
