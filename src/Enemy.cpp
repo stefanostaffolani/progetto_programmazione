709d3b3339d09a05f8e9598e4eeb4e92918ae806
stefanostaffolani <staffolani.stefano@gmail.com>
2022-01-14 23:46:49 +0100

    pulizia

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -5,8 +5,8 @@ Enemy::Enemy(char avatar, Platform* p1, position pos, int type = 0, bool on_plat
     set_damage();
     p2 = p1;
     this->on_plat = on_plat;
-    if(type == 0) life = 20;
-    else life = 50;
+    if(type == 0) life = BASE_ENEMY_LIFE;    // setto la vita in base al tipo
+    else life = HARD_ENEMY_LIFE;
 }
 
 void Enemy::decrease_life(int n){this->life -= n;}
@@ -14,22 +14,21 @@ void Enemy::decrease_life(int n){this->life -= n;}
 int Enemy::get_life(){return this->life;}
 
 void Enemy::set_damage(){ 
-    if(this->type == 0) this->damage = 10;   // nemico base
-    else this->damage = 20;                     // nemico forte
+    if(this->type == 0) this->damage = P_DAMAGE;   // nemico base
+    else this->damage = 2*P_DAMAGE;                     // nemico forte
 }
 
 int Enemy::get_damage(){return this->damage;}
 
 int Enemy::get_type(){return this->type;}
 
-void Enemy::random_move(int ps){  //TODO: controllare questa funzione sulle platform
+void Enemy::random_move(int ps){
     if(this->type > 0){
-        mvprintw(22,1,"%d,%d",pos.x,pos.y);
         int random_dir = rand()%2;     //if random_dir == 0 va a dx else a sx, se può
         if(random_dir) this->set_versor(-1);
         else this->set_versor(1);
-        if(mvinch(this->pos.y, this->pos.x + this->versor - ps) == (int)' '){
-            timeout(100);
+        if(mvinch(this->pos.y, this->pos.x + this->versor - ps) == (int)' '){    // si sposta se non ha nulla di fianco
+            timeout(100);                   // si sposta una volta ogni ciclo di gioco
             if(this->on_plat && !this->check_plat_border(ps)){
                 this->move(ps);
             }else if (!this->on_plat){
@@ -42,7 +41,7 @@ void Enemy::random_move(int ps){  //TODO: controllare questa funzione sulle plat
 void Enemy::random_shoot(int freq, int x_player, int ps, Shoot* s2){   // freq = 10 - difficoltà
     if(this->type > 0){         //sparano solo i nemici forti
         int dir_shoot;
-        if((x_player - this->pos.x + ps) < 0) dir_shoot = -1;
+        if((x_player - this->pos.x + ps) < 0) dir_shoot = -1;   // la direzione di sparo e' verso il player]
         else dir_shoot = 1;
         int n = rand() % freq;      // se aumenta la frequenza aumenta la probabilità di sparare
         position traslated_position = {this->pos.x-ps, this->pos.y};
