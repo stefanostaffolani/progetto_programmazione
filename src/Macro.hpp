64b2b54fd1a38262d32ff0301611f8d33d6038c6
stefanostaffolani <staffolani.stefano@gmail.com>
2022-01-17 21:43:04 +0100

    update macro

--- src/Macro.hpp
+++ src/Macro.hpp
@@ -3,9 +3,6 @@
 #define HEIGHT 12
 #define LENGTH 75
 #define SPACE 32
-#define EQUAL 61
-#define DOLLAR 36
-#define PIPE 124
 #define P_DAMAGE 10
 #define LIFE 100
 #define BASE_ENEMY_LIFE 20
@@ -15,3 +12,4 @@
 #define ENEM_x_POS 50
 #define BONU_x_POS 30 
 #define PLAT_X_POS 40 
+#define Y_JUMP 5
